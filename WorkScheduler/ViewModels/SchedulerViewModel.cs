c217a0e6c5493d3c44ade7b1c2fe807e15b30af5
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2023-01-25 19:49:41 +0900

    add input screen

---
+++ WorkScheduler/ViewModels/SchedulerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.Maui.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkScheduler.ViewModels
+{
+    public class SchedulerViewModel
+    {
+        public ObservableCollection<SchedulerAppointment> SchedulerEvents { get; set; }
+        public SchedulerViewModel()
+        {
+            SchedulerEvents = new ObservableCollection<SchedulerAppointment>
+            {
+                new SchedulerAppointment {StartTime=new DateTime(2023, 1, 23, 18,0,0),EndTime=new DateTime(2023, 1, 23, 20, 0,0),Subject="テスト"}
+            };
+        }
+    }
+}
