91997bf25ca0d6030e2f09d9f064787e83d0b650
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2023-01-26 00:16:57 +0900

    OnSchedulerDoubleTappedをコードビハインドで実装

--- WorkScheduler/ViewModels/SchedulerViewModel.cs
+++ WorkScheduler/ViewModels/SchedulerViewModel.cs
@@ -12,12 +12,26 @@ namespace WorkScheduler.ViewModels
 {
     internal partial class SchedulerViewModel : ObservableObject
     {
+        //public event EventHandler<SchedulerDoubleTappedEventArgs> Change;
+
         public ObservableCollection<SchedulerAppointment> SchedulerEvents { get; set; } = new ObservableCollection<SchedulerAppointment>();
+
+        public SchedulerViewModel()
+        {
+            //Change += OnSchedulerDoubleTapped;
+        }
 
         [RelayCommand]
         private void AddNewSchedule()
         {
             SchedulerEvents.Add(new SchedulerAppointment { StartTime = new DateTime(2023, 1, 25, 18, 0, 0), EndTime = new DateTime(2023, 1, 25, 20, 0, 0), Subject = "テスト" });
+        }
+
+        private void OnSchedulerDoubleTapped(object sender, SchedulerDoubleTappedEventArgs e)
+        {
+            var appointments = e.Appointments;
+            var selectedDate = e.Date;
+            var schedulerElement = e.Element;
         }
     }
 }
--- WorkScheduler/Views/SchedulerView.xaml.cs
+++ WorkScheduler/Views/SchedulerView.xaml.cs
@@ -1,9 +1,19 @@
+using Syncfusion.Maui.Scheduler;
+
 namespace WorkScheduler.Views;
 
 public partial class SchedulerView : ContentPage
 {
-	public SchedulerView()
-	{
-		InitializeComponent();
-	}
+    public SchedulerView()
+    {
+        InitializeComponent();
+        Scheduler.DoubleTapped += this.OnSchedulerDoubleTapped;
+    }
+
+    private void OnSchedulerDoubleTapped(object sender, SchedulerDoubleTappedEventArgs e)
+    {
+        var appointments = e.Appointments;
+        var selectedDate = e.Date;
+        var schedulerElement = e.Element;
+    }
 }
