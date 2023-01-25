385b1ec733bdbf5e51a5dffd080537a73150875e
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2023-01-25 20:36:53 +0900

    commit

--- WorkScheduler/ViewModels/SchedulerViewModel.cs
+++ WorkScheduler/ViewModels/SchedulerViewModel.cs
@@ -14,10 +14,28 @@ namespace WorkScheduler.ViewModels
     {
         public ObservableCollection<SchedulerAppointment> SchedulerEvents { get; set; } = new ObservableCollection<SchedulerAppointment>();
 
+        [ObservableProperty]
+        private DateTime _displayDate;
+
+        [ObservableProperty]
+        private DateTime _selectedDate;
+
+
+        public SchedulerViewModel()
+        {
+        }
+
         [RelayCommand]
         private void AddNewSchedule()
         {
-            SchedulerEvents.Add(new SchedulerAppointment { StartTime = new DateTime(2023, 1, 25, 18, 0, 0), EndTime = new DateTime(2023, 1, 25, 20, 0, 0), Subject = "テスト" });
+            SchedulerEvents.Add(new SchedulerAppointment { StartTime = new DateTime(2023, 1, 25, 18, 0, 0), EndTime = new DateTime(2023, 1, 25, 20, 0, 0), Subject = "テスト", Background = Colors.Green });
+        }
+
+        public void OnSchedulerDoubleTapped(object sender, SchedulerDoubleTappedEventArgs e)
+        {
+            var appointments = e.Appointments;
+            var selectedDate = e.Date;
+            var schedulerElement = e.Element;
         }
     }
 }
