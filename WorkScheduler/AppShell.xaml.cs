198c27f79ca364f282ed578bb20b0c9c54159047
Shotaro Nakamura <shotaro.nakamura.j8f@denso-wave.com>
2023-01-31 18:52:07 +0900

    画面遷移実装

--- WorkScheduler/AppShell.xaml.cs
+++ WorkScheduler/AppShell.xaml.cs
@@ -18,6 +18,7 @@ public partial class AppShell : Shell
         Routes.Add(nameof(SchedulerView), typeof(SchedulerView));
         Routes.Add(nameof(InputDetails), typeof(InputDetails));
         Routes.Add(nameof(MainPage), typeof(MainPage));
+        Routes.Add(nameof(HamburgerMenu), typeof(HamburgerMenu));
 
 
         foreach (var item in Routes)
