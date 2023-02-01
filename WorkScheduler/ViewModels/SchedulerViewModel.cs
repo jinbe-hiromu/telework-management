96735436cbf1e2313a54e3ee4cafb772a22e8110
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2023-02-02 07:08:36 +0900

    Add DeleteSchedule

--- WorkScheduler/ViewModels/SchedulerViewModel.cs
+++ WorkScheduler/ViewModels/SchedulerViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using RestSharp;
 using Syncfusion.Maui.Scheduler;
 using System.Collections.ObjectModel;
-using System.Net.Http.Json;
-using System.Text.Json;
+using System.Diagnostics;
+using WorkScheduler.Models;
 using WorkScheduler.Views;
 
 namespace WorkScheduler.ViewModels
@@ -14,6 +15,8 @@ namespace WorkScheduler.ViewModels
     {
         private string _accessToken = "czWjvBwr4eWYX32ZZsJGhw==";
         private RestClient _client = new RestClient("http://localhost:5000");
+        private IList<DateTime> _visibleDates;
+        private SchedulerAppointment _selectedAppointment;
 
         public SchedulerViewModel()
         {
@@ -50,9 +53,11 @@ namespace WorkScheduler.ViewModels
         {
             if (e is not null)
             {
-                var appointments = e.Appointments;
-                var selectedDate = e.Date;
-                var schedulerElement = e.Element;
+                _selectedAppointment = (SchedulerAppointment)e.Appointments.First();
+            }
+            else
+            {
+                _selectedAppointment = null;
             }
         }
 
@@ -60,30 +65,54 @@ namespace WorkScheduler.ViewModels
         {
             if (e is not null)
             {
-                foreach (var visibleDate in e.NewVisibleDates)
+                _visibleDates = e.NewVisibleDates;
+                await UpdateScheduler(e.NewVisibleDates);
+            }
+        }
+
+        private async Task UpdateScheduler(IList<DateTime> targetDates)
+        {
+            SchedulerEvents.Clear();
+
+            foreach (var targetDate in targetDates)
+            {
+                var request = new RestRequest($"/api/workschedule/{targetDate.Year}/{targetDate.Month}/{targetDate.Day}");
+                request.AddHeader("AccessToken", _accessToken);
+
+                try
                 {
-                    var request = new RestRequest($"/api/workschedule/{visibleDate.Year}/{visibleDate.Month}/{visibleDate.Day}");
-                    request.AddHeader("AccessToken", _accessToken);
-
+                    // 予定が0件の場合もExceptionが発生するため。
                     var response = await _client.GetAsync(request);
-
-                    if (!response.IsSuccessStatusCode)
+                    // 予定が1件の場合非配列JSON、予定が複数件の場合配列型JSON
+                    if (response.Content[0] == '[')
                     {
-                        var toast = CommunityToolkit.Maui.Alerts.Toast.Make("StatusCode:" + response.StatusCode);
-                        _ = toast.Show(CancellationToken.None);
-                        continue;
+                        var schedules = JsonConvert.DeserializeObject<List<Schedule>>(response.Content);
+                        foreach (var schedule in schedules)
+                        {
+                            SchedulerEvents.Add(new SchedulerAppointment
+                            {
+                                StartTime = schedule.StartTime,
+                                EndTime = schedule.EndTime,
+                                Subject = CreateSubject(schedule.WorkStyle, schedule.WorkingPlace),
+                            });
+                        }
+                    }
+                    else
+                    {
+                        var schedule = JsonConvert.DeserializeObject<Schedule>(response.Content);
+                        SchedulerEvents.Add(new SchedulerAppointment
+                        {
+                            StartTime = schedule.StartTime,
+                            EndTime = schedule.EndTime,
+                            Subject = CreateSubject(schedule.WorkStyle, schedule.WorkingPlace),
+                        });
                     }
 
-                    //var jsonString = response.Content.ReadAsStringAsync().Result;
-
-                    //var eventData = JsonSerializer.Deserialize<EventInfo>(jsonString);
-
-                    //SchedulerEvents.Add(new SchedulerAppointment
-                    //{
-                    //    StartTime = DateTime.Parse(eventData.StartTime),
-                    //    EndTime = DateTime.Parse(eventData.EndTime),
-                    //    Subject = $"{eventData.WorkStyle}[{eventData.WorkingPlace}]",
-                    //});
+                }
+                catch (Exception ex)
+                {
+                    // Do nothing
+                    Debug.WriteLine(ex);
                 }
             }
         }
@@ -94,10 +123,7 @@ namespace WorkScheduler.ViewModels
             var result = await Shell.Current.ShowPopupAsync(new InputDetails());
             if (result is InputDetailsContact output)
             {
-                var str = $"{nameof(output.Date)}: {output.Date}{Environment.NewLine}{nameof(output.StartTime)}: {output.StartTime}{Environment.NewLine}{nameof(output.EndTime)}: {output.EndTime}{Environment.NewLine}{nameof(output.WorkStyle)}: {output.WorkStyle}{Environment.NewLine}{nameof(output.WorkingPlace)}: {output.WorkingPlace}{Environment.NewLine}";
-
-
-                var requestBody = new EventInfo
+                var eventInfo = new EventInfo
                 {
                     Date = output.Date.ToString("yyyy-MM-dd"),
                     StartTime = output.Date.ToString("yyyy-MM-dd") + "T" + output.StartTime.ToString(@"hh\:mm"),        // Format:"2023-01-30T08:40"
@@ -105,31 +131,50 @@ namespace WorkScheduler.ViewModels
                     WorkStyle = output.WorkStyle,
                     WorkingPlace = output.WorkingPlace,
                 };
-                //var response = await _client.PostAsJsonAsync($"{_host}/api/workschedule/{output.Date.Year}/{output.Date.Month}/{output.Date.Day}", requestBody);
 
-
-                //if (!response.IsSuccessStatusCode)
-                //{
-                //    var toast = CommunityToolkit.Maui.Alerts.Toast.Make("StatusCode:" + response.StatusCode);
-                //    _ = toast.Show(CancellationToken.None);
-                //}
+                var request = new RestRequest($"/api/workschedule/{output.Date.Year}/{output.Date.Month}/{output.Date.Day}");
+                request.AddHeader("AccessToken", _accessToken);
+                request.AddBody(eventInfo);
+                try
+                {
+                    var response = await _client.PostAsync(request);
+                    await UpdateScheduler(_visibleDates);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
             }
         }
 
 
         [RelayCommand]
-        private void EditScheduler()
+        private void EditSchedule()
         {
 
         }
 
 
         [RelayCommand]
-        private void DeleteScheduler()
+        private async void DeleteSchedule()
         {
-
+            if (_selectedAppointment is not null)
+            {
+                var request = new RestRequest($"/api/workschedule/{_selectedAppointment.StartTime.Year}/{_selectedAppointment.StartTime.Month}/{_selectedAppointment.StartTime.Day}");
+                request.AddHeader("AccessToken", _accessToken);
+                await _client.DeleteAsync(request);
+                await UpdateScheduler(_visibleDates);
+            }
         }
 
+        private string CreateSubject(string workStyle, string workingPlace)
+        {
+            if (string.IsNullOrEmpty(workingPlace))
+            {
+                return $"{workStyle}";
+            }
+            return $"{workStyle}[{workingPlace}]";
+        }
     }
 
     internal class EventInfo
