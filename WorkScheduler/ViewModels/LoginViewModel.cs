a31112af0c0704236ed41e654bf119e9c709421b
hirota touma <hirota.touma.j5s@denso-wave.com>
2023-02-02 16:57:40 +0900

    APIアクセス処理追加

--- WorkScheduler/ViewModels/LoginViewModel.cs
+++ WorkScheduler/ViewModels/LoginViewModel.cs
@@ -1,11 +1,18 @@
 ﻿
+using CommunityToolkit.Maui.Alerts;
+using RestSharp;
+using System.Linq.Expressions;
 using WorkScheduler.Views;
 
 namespace WorkScheduler.ViewModels
 {
     public class LoginViewModel : BindableObject
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public Command LoginCommand { get; }
+
+        private RestClient _client = new RestClient("http://localhost:5000");
 
         public LoginViewModel()
         {
@@ -14,7 +21,22 @@ namespace WorkScheduler.ViewModels
 
         private async void OnLoginClicked(object _)
         {
-            await Shell.Current.GoToAsync(nameof(MainPage));
+            try
+            {
+                var request = new RestRequest($"/api/WorkSchedule/Login");
+                request.AddBody("username", UserName);
+                request.AddBody("password", Password);
+                //var accessToken = await _client.PostAsync(request); 
+                var accessToken = "czWjvBwr4eWYX32ZZsJGhw==";
+                var parameter = new Dictionary<string, object>() { { "AccessToken", accessToken } };
+                await Shell.Current.GoToAsync(nameof(MainPage), parameter);
+            }
+            catch
+            {
+                var toast = Toast.Make("ユーザ名かパスワードが間違っています。", CommunityToolkit.Maui.Core.ToastDuration.Short, 14);
+                await toast.Show().ConfigureAwait(false);
+            }
+
         }
     }
 }
