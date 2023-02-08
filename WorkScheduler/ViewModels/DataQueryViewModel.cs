3f686d45bcca569ceaad794941b57e535db46d37
takuya yamauchi <takuya.yamauchi.j7w@denso-wave.com>
2023-02-08 22:11:00 +0900

    https対応

--- WorkScheduler/ViewModels/DataQueryViewModel.cs
+++ WorkScheduler/ViewModels/DataQueryViewModel.cs
@@ -6,15 +6,16 @@ namespace WorkScheduler.ViewModels;
 public partial class DataQueryViewModel : ObservableObject
 {
     [ObservableProperty]
-    private UrlWebViewSource _source;
-    [ObservableProperty]
     private CookieContainer _cookies;
-
-    public DataQueryViewModel() : this(new CookieContainer()) { }
+    [ObservableProperty]
+    private UrlWebViewSource _source;
 
     public DataQueryViewModel(CookieContainer cookies)
     {
-        _source = new UrlWebViewSource { Url = $"http://localhost:5000/api/WorkSchedule/2023/2/3?accesskey={cookies.GetCookieHeader(new Uri("http://localhost:5000"))}" };
         _cookies = cookies;
+        _source = new UrlWebViewSource
+        {
+            Url = $"http://localhost:5000/api/WorkSchedule/2023/2/3?accesskey={cookies.GetCookieHeader(new Uri("https://localhost:5001"))}"
+        };
     }
 }
