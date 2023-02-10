33bf40f51ee36203f428a64ba72649b2bb38d86a
takuya yamauchi <takuya.yamauchi.j7w@denso-wave.com>
2023-02-10 11:57:30 +0900

    Cookie使用WebView追加

--- WorkScheduler/ViewModels/DataQueryViewModel.cs
+++ WorkScheduler/ViewModels/DataQueryViewModel.cs
@@ -6,32 +6,14 @@ namespace WorkScheduler.ViewModels;
 public partial class DataQueryViewModel : ObservableObject
 {
     [ObservableProperty]
-    private CookieContainer _cookies;
+    private string _cookie;
 
     [ObservableProperty]
-    private UrlWebViewSource _source;
+    private string _url;
 
     public DataQueryViewModel(CookieContainer cookies)
     {
-        _cookies = cookies.Copy();
-        _source = new UrlWebViewSource
-        {
-            Url = "http://localhost:5000/work-schedule-view"
-        };
-    }
-}
-
-internal static class CookieContainerExtensions
-{
-    internal static CookieContainer Copy(this CookieContainer source)
-    {
-        var destination = new CookieContainer(source.Capacity,
-                                              source.PerDomainCapacity,
-                                              source.MaxCookieSize);
-
-        var cookies = source.GetAllCookies();
-        destination.Add(cookies);
-
-        return destination;
+        _cookie = cookies.GetCookieHeader(new Uri("http://localhost"));
+        _url = "http://localhost:5000/";
     }
 }
---
+++ WorkScheduler/Views/Controls/WebViewWithCookie.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Maui.Handlers;
+
+namespace WorkScheduler.Views.Controls;
+
+public class WebViewWithCookie : WebView
+{
+    static WebViewWithCookie()
+    {
+        WebViewHandler.Mapper.AppendToMapping(nameof(IWebView.Source),
+#if WINDOWS
+			async
+#endif
+            (handler, view) =>
+        {
+            if (view is WebViewWithCookie webView)
+            {
+                var url = webView.Url!;
+#if ANDROID
+                var headers = new Dictionary<string, string>
+                {
+                    ["Cookie"] = webView.Cookie
+                };
+                handler.PlatformView.LoadUrl(url, headers);
+#elif iOS
+                var webRequest = new NSMutableUrlRequest(new NSUrl(url));
+                var headerKey = new NSString("Cookie");
+                var headerValue = new NSString(webView.Cookie);
+                var dictionary = new NSDictionary(headerKey, headerValue);
+                webRequest.Headers = dictionary;
+
+                handler.PlatformView.LoadRequest(webRequest);
+#elif WINDOWS
+		        await handler.PlatformView.EnsureCoreWebView2Async();
+
+			    var request = handler.PlatformView.CoreWebView2.Environment.CreateWebResourceRequest(
+				    uri: url,
+				    Method: "GET",
+				    postData: Stream.Null.AsRandomAccessStream(),
+				    Headers: null);
+
+			    request.Headers.SetHeader("Cookie", webView.Cookie);
+			    //request.Headers.SetHeader("Connection", "");
+			    request.Headers.SetHeader("Accept-Language", "ja,en;q=0.9,en-GB;q=0.8,en-US;q=0.7");
+
+			    handler.PlatformView.CoreWebView2.NavigateWithWebResourceRequest(request);
+#else
+                throw new NotImplementedException();
+#endif
+            }
+
+        });
+    }
+
+    public static readonly BindableProperty CookieProperty = BindableProperty.Create(nameof(Cookie), typeof(string), typeof(WebViewWithCookie), "");
+
+    public string Cookie
+    {
+        get => GetValue(CookieProperty) as string;
+        set => SetValue(CookieProperty, value);
+    }
+
+    public static readonly BindableProperty UrlProperty = BindableProperty.Create(nameof(Url), typeof(string), typeof(WebViewWithCookie), "");
+    public string Url
+    {
+        get => GetValue(UrlProperty) as string;
+        set => SetValue(UrlProperty, value);
+    }
+}
