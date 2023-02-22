100154aa610fab602c62cf8290a130eb45bb949a
jinbe- <matsubara0501jp@gmail.com>
2023-02-22 23:15:50 +0900

    commit

--- WorkScheduler/Services/NavigationService.cs
+++ WorkScheduler/Services/NavigationService.cs
@@ -1,14 +1,17 @@
-﻿using WorkScheduler.Views;
+﻿using CommunityToolkit.Maui.Views;
+using WorkScheduler.Models;
+using WorkScheduler.Views;
 
 namespace WorkScheduler.Services;
 
 public interface INavigationService
 {
+    Task NavigateToLogin();
     Task NavigateToMain();
     Task NavigateToDataQuery();
     Task NavigateTo<T>() where T : Page;
-    T ResolvePage<T>() where T : Page;
     Page ResolvePage(Type type);
+    T ResolvePage<T>() where T : Page;
 }
 
 internal class NavigationService : INavigationService
@@ -22,9 +25,24 @@ internal class NavigationService : INavigationService
         _services = services;
     }
 
-    public Task NavigateToDataQuery() => NavigateTo<DataQueryView>();
+    public Task NavigateToLogin() => NavigateTo<LoginView>();
 
     public Task NavigateToMain() => NavigateTo<MainPage>();
+
+    public Task NavigateToDataQuery() => NavigateTo<DataQueryView>();
+
+    public Task<InputDetailsContact> PopupTo<InputDetails>()
+    {
+        if(DeviceInfo.Platform == DevicePlatform.WinUI || DeviceInfo.Platform == DevicePlatform.MacCatalyst)
+        {
+            return PopupTo<>
+        }
+        else
+        {
+
+        }
+    }
+
 
     public Task NavigateTo<T>() where T : Page
     {
@@ -32,7 +50,15 @@ internal class NavigationService : INavigationService
         return Navigation.PushAsync(page, true);
     }
 
+    public Task PopupTo<T>() where T : Popup
+    {
+        var popup = ResolvePopup<T>();
+        return Navigation.PopAsync(popup);
+    }
+
     public Page ResolvePage(Type type) => _services.GetService(type) as Page;
 
     public T ResolvePage<T>() where T : Page => _services.GetService<T>();
+
+    public T ResolvePopup<T>() where T : Popup => _services.GetService<T>();
 }
--- WorkScheduler/ViewModels/LogoutViewModel.cs
+++ WorkScheduler/ViewModels/LogoutViewModel.cs
@@ -1,25 +1,27 @@
 ﻿using WorkScheduler.Models;
+using WorkScheduler.Services;
 
 namespace WorkScheduler.ViewModels
 {
     public partial class LogoutViewModel : BindableObject
     {
         private readonly IWorkSchedulerClient _client;
+        private readonly INavigationService _navigationService;
 
         public Command LogoutCommand { get; }
 
-        public LogoutViewModel(IWorkSchedulerClient client)
+        public LogoutViewModel(IWorkSchedulerClient client, INavigationService navigationService)
         {
             _client = client;
+            _navigationService = navigationService;
 
             LogoutCommand = new(OnLogoutClicked);
         }
 
         private async void OnLogoutClicked(object _)
         {
-            //ログアウト処理
             await _client.LogoutAsync();
-            await Shell.Current.GoToAsync("..");
+            await _navigationService.NavigateToLogin();
         }
 
     }
